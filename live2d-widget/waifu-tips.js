f9e677134191f38f163fa4c89ad6baa375c34f97
卓 <creator.chen@foxmail.com>
2021-08-25 17:59:59 +0800

    Site updated: 2021-08-25 17:59:59

--- live2d-widget/waifu-tips.js
+++ live2d-widget/waifu-tips.js
@@ -163,7 +163,7 @@ function loadWidget(config) {
 		if (modelId === null) {
 			// 首次访问加载 指定模型 的 指定材质
 			modelId = 4; // 模型 ID
-			modelTexturesId = 20; //53 材质 ID
+			modelTexturesId = 1; // 材质 ID
 		}
 		loadModel(modelId, modelTexturesId);
 		fetch(waifuPath)
