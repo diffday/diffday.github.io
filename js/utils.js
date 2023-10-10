02d0b807f0068563495d3c25132749a5070a4161
卓 <creator.chen@foxmail.com>
2023-10-10 23:52:37 +0800

    Site updated: 2023-10-10 23:52:36

--- js/utils.js
+++ js/utils.js
@@ -1106,7 +1106,7 @@ const anzhiyu = {
   // 跳转开往
   totraveling: function () {
     anzhiyu.snackbarShow(
-      "即将跳转到「开往」项目的成员博客，不保证跳转网站的安全性和可用性",
+      "即将跳转到「开往」项目的成员网站，不保证跳转页的安全性和可用性",
       element => {
         element.style.opacity = 0;
         travellingsTimer && clearTimeout(travellingsTimer);
