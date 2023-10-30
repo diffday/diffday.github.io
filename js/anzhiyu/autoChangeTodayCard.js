2962c73ed637ae2d947a9e3d81db2cb271f0fe4e
卓 <creator.chen@foxmail.com>
2023-10-31 00:04:44 +0800

    Site updated: 2023-10-31 00:04:44

--- js/anzhiyu/autoChangeTodayCard.js
+++ js/anzhiyu/autoChangeTodayCard.js
@@ -8,14 +8,38 @@ function autoChangeTodayCard() {
 
 document.addEventListener("DOMContentLoaded",autoChangeTodayCard);
 //浏览器后退不会触发pageshow事件
-//window.addEventListener("pageshow",autoChangeTodayCard);
+//window.addEventListener("pageshow",function(event) {console.log(event)});
+//window.addEventListener("load",function(event) {console.log(event)});
 
 //后退前进事件
-window.addEventListener("popstate",function(event) {
-	if (event.state.url.lastIndexOf("/") == event.state.url.length - 1) {
-		window.setTimeout(() => {
-			//event.target.autoChangeTodayCard();
-			autoChangeTodayCard();
-		}, "500");
+//window.addEventListener("popstate",function(event) {
+	//console.log(event);
+	//if (event.state.url.lastIndexOf("/") == event.state.url.length - 1) {
+		//window.setTimeout(() => {
+			//--event.target.autoChangeTodayCard();
+			//autoChangeTodayCard();
+		//}, "500");
+	//}
+//});
+
+function checkLocationRefresh() {
+	let p = window.location.pathname == '/';
+	let s = document.readyState == 'complete';
+	if(p && s){
+		autoChangeTodayCard();
 	}
-});
+}
+
+window.setInterval(checkLocationRefresh, "1000");
+
+//window.addEventListener("pjax:start",function(event) {
+	//console.log(event);
+//});
+
+//window.addEventListener("ready",function(event) {
+	//console.log(event);
+//});
+
+//window.addEventListener("visibilitychange",function(event) {
+	//console.log(event);
+//});
